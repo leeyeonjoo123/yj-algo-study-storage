887ff584ac19571a321f55859fb46ca837dfaac4
이연주 <132028329+leeyeonjoo123@users.noreply.github.com>
2023-11-29 19:07:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int y = scanner.nextInt();
+        int ad = y - 543;
+        System.out.println(ad);
+
+        scanner.close();
+    }
+}
