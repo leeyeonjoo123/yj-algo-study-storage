df7847532faa3ed27ec01bea9f12f7f445f4147e
이연주 <132028329+leeyeonjoo123@users.noreply.github.com>
2024-01-22 20:48:34 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.lang.*;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /*
 로직.
@@ -20,19 +21,27 @@ public class Main {
         // 테스트 케이스의 개수 T
         int T = sc.nextInt();
         //System.out.println(T);
-
+        int[] A = new int[T];
+        int[] B = new int[T]; //길이 T인 배열
         // 테스트 케이스(0부터 테스트 케이스 수만큼 반복)
-        // 왜 T 자리에 무슨 수를 넣어도 네번째가지만 반복하지? 
+
+        // 입력
         for (int i = 0; i < T ; i++) {
             //for문 돌때마다 A, B 생성 및 저장
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+            A[i] = sc.nextInt();
+            B[i] = sc.nextInt();
 
+//            sc.nextLine();
+//            System.out.println( "A : " + A + "  B : " + B + " i : " + i);
             // 입력 조건 만족하는지 확인
-            checkInput(A, B);
+//            checkInput(A[i], B[i]);
 
-            //System.out.println( "A : " + A + "  B : " + B + " i : " + i);
-            System.out.println(A+B);
+//            System.out.println(A[i+B);
+        }
+
+        //출력
+        for(int i=0; i<T; i++){
+            System.out.println(A[i]+B[i]);
         }
 
         sc.close();
@@ -41,6 +50,7 @@ public class Main {
     // 입력 조건에 맞지 않은 값이 입력되면 프로그램 종료
     public static void checkInput(int A, int B) {
         if ((A < 1 || A > 10) || (B < 1 || B > 10)) {
+            System.out.println(A+" "+B);
             System.exit(0);
         }
     }
