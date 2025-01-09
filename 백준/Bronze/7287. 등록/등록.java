260b36c5ba537efaabf146c4109c310a5937fcd9
leeyeonjoo123 <132028329+leeyeonjoo123@users.noreply.github.com>
2025-01-09 16:36:44 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int rightEx = 60;
+		String ID = "95162tree";
+		System.out.println(rightEx);
+		System.out.println(ID);
+	}
+
+}
