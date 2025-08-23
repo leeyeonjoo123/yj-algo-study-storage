f787e130f2211450bee47062a1d959e0ef9f05d1
leeyeonjoo123 <132028329+leeyeonjoo123@users.noreply.github.com>
2025-08-23 14:17:54 +0900

    [Bronze V] Title: N 찍기, Time: 824 ms, Memory: 31764 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sc = scanner.nextInt();
+        //
+        for(int i = 1 ; i <= sc ; i++){
+            System.out.println(i);
+        }
+    }
+}
