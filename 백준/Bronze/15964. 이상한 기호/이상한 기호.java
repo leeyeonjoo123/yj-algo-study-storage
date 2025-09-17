04028aeaedee2b14fb429087a6840726aedf7aa4
leeyeonjoo123 <132028329+leeyeonjoo123@users.noreply.github.com>
2025-09-17 10:55:51 +0900

    [Bronze V] Title: 이상한 기호, Time: 176 ms, Memory: 17684 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,19 +1,13 @@
 import java.util.*;
 
 public class Main {
-
-	public static int cal(int A, int B){
-		int result = (A+B)*(A-B);
-		return result;
-	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		System.out.println(cal(A,B));
+		Long A = sc.nextLong();
+		Long B = sc.nextLong();
+		System.out.println((A+B)*(A-B));
 		sc.close();
-		
 	}
 }
